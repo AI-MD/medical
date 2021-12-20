72487bf4f43ba7f33ef9888d471baac526269d52
donghwan1994 <donghwan1994@kangwon.ac.kr>
2021-12-20 07:32:45 +0000

    Update ce.py

--- ce.sh
+++ ce.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-python multi_class.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 0 --log logs/CE
-python multi_class.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 1 --log logs/CE
-python multi_class.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 2 --log logs/CE
+python ce.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 0 --log logs/CE
+python ce.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 1 --log logs/CE
+python ce.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 2 --log logs/CE
