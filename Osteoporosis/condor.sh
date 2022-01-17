64c8391aa40418ee58633824d9cc13376a906041
jungwhan <k2p2047@naver.com>
2022-01-17 15:39:38 +0900

    new update

--- Osteoporosis/condor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-python condor.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 0 --log logs/CONDOR
-python condor.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 1 --log logs/CONDOR
-python condor.py /root/dataset/Osteoporosis/Sagittal_Contrast_Final_1/ --n_gpu 4 -b 32 --lr 0.0001 -j 16 --epochs 60 --seed 2 --log logs/CONDOR
