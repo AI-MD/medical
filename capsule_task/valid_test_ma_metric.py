5cd1db9e6b3023f43bd01a0ac8d7b970eee1d719
jungwhan <k2p2047@naver.com>
2022-04-28 17:36:31 +0900

    change post processing metric

---
+++ capsule_task/valid_test_ma_metric.py
@@ -0,0 +1,256 @@
+import os
+import argparse
+import cv2
+import torch
+
+import model.model as module_arch
+from parse_config import ConfigParser
+
+from torchvision import transforms
+from PIL import Image
+from pathlib import Path
+
+from utils import prepare_device
+import csv
+import pathlib
+import numpy as np
+
+import sklearn.metrics as metrics
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+def moving_average(arr, window_size):
+    i = 0
+    # Initialize an empty list to store moving averages
+    moving_averages = []
+    
+    # Loop through the array t o
+    #consider every window of size 3
+    while i < len(arr) - window_size + 1:
+    
+        # Calculate the average of current window
+        window_average = round(np.sum(arr[
+        i:i+window_size]) / window_size, 2)
+        
+        # Store the average of current
+        # window in moving average list
+        moving_averages.append(window_average)
+        
+        # Shift window to right by one position
+        i += 1
+
+    return moving_averages
+
+def getLabelMap(config):
+    e = open(config['label_info'], 'r')
+    labeltmap= csv.DictReader(e)
+    return labeltmap
+
+def main(config):
+    logger = config.get_logger('test')
+
+    # Preprocessing transformations
+    preprocess = transforms.Compose([
+        transforms.CenterCrop((512, 512)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.3960, 0.2987, 0.3811], std=[0.1301, 0.1792, 0.1275]),
+    ])
+
+    device, device_ids = prepare_device(config['n_gpu'])
+
+    # build model architecture, then print to console
+    CRNN_model = config.init_obj('crnn_arch', module_arch, device=device)
+
+    logger.info('Loading checkpoint: {} ...'.format(config['test_resume']))
+    checkpoint = torch.load(config['test_resume'])
+    state_dict = checkpoint['state_dict']
+
+    CRNN_model.load_state_dict(state_dict, strict=False)
+    if len(device_ids) > 1:
+        CRNN_model = torch.nn.DataParallel(CRNN_model, device_ids=device_ids)
+
+    CRNN_model = CRNN_model.to(device)
+    CRNN_model.eval()
+
+    label_map = getLabelMap(config)
+    
+
+
+    label_list = {"D2010":[653,	8361,	57816],
+                "D2021": [592,	10460,	65405],
+                 "KNUH6060":[160,	2573,	69535],
+                 "KNUH6075":[160,	5949,	86669],
+                "case26": [407,	5022,	100220],
+                 "case28":[64,	13985,	67035],
+                 "case29":[210,	3609,	47700],
+                 "case30":[1,	8870,	45249],
+                 "duh1":[400,	5745,	50375],
+                "duh2": [137,	3110,	94200]
+                }
+
+
+    with torch.no_grad():
+        for root, _, fnames in sorted(os.walk(config['video_path'], followlinks=True)):
+            path_list = sorted(fnames)
+           
+
+            for num, fname in enumerate(path_list):
+                path = os.path.join(root, fname)
+                
+                filename = "./valid_mean_result/"+fname.split('.')[0] + "_resultforRoc_0427.csv"
+
+                f = open(filename, 'w', newline='')
+                wr = csv.writer(f)
+                wr.writerow(['frame_index','predtict' , 'label' ])
+
+                
+                filename_new = "./valid_mean_result/"+fname.split('.')[0] + "_resultforCount_0427.csv"
+
+                f_n = open(filename_new, 'w', newline='')
+                wr_n = csv.writer(f_n)
+                wr_n.writerow(["index", "0_mean" , "1_mean" , "2_mean" , "label"])
+
+
+                cap = cv2.VideoCapture(path)
+                fps = cap.get(cv2.CAP_PROP_FPS)
+                frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 영상의 넓이(가로) 프레임
+                frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 영상의 높이(세로) 프레임
+
+                frame_size = (frameWidth, frameHeight)
+
+                #out = cv2.VideoWriter(os.path.join("./", fname), fcc, fps, frame_size)
+               
+                pred_count = [0, 0, 0]
+                pred_prob = []
+                pred_prob_list =[]
+
+                frame_index = 0
+                pred_prob =[]
+                
+                case_label =label_list.get(fname.split('.')[0])
+               
+                while True:
+                    retval, frame = cap.read()
+                    frame_index = int(frame_index) + 1
+                    y_label = 0
+                    
+                    if frame_index < case_label[0]:
+                        continue
+                    elif frame_index >= case_label[0] and frame_index < case_label[1]:
+                       y_label = 0
+                    elif frame_index >= case_label[1] and frame_index < case_label[2]:
+                       y_label = 1
+                    else:
+                       y_label = 2
+
+                    if not (retval):  # 프레임정보를 정상적으로 읽지 못하면
+                        break  # while문을 빠져나가기
+
+                    pil_src = Image.fromarray(frame)
+                    inputs = preprocess(pil_src).unsqueeze(0)
+
+                    inputs_torch = inputs.unsqueeze(0).to(device)
+                    output = CRNN_model(inputs_torch)
+                    outputs = torch.softmax(output, dim=2)
+                    
+                    output = outputs.reshape(outputs.size(0) * outputs.size(1), -1)  # (batch * seq_len x classes)
+                    predicted = torch.argmax(output,dim = 1)
+
+                    wr_list = []
+                    wr_list.append(frame_index)
+                    wr_list.append(predicted.item())
+                    wr_list.append(y_label)
+                    wr.writerow(wr_list)
+                    pred_score = output.cpu().numpy().squeeze()
+                    pred_prob.append(pred_score)
+                    
+                    
+                    if len(pred_prob) == config['clip_num']:
+       
+                        pred_prob_array = np.array(pred_prob)
+                      
+                        print("--------------------------------")
+                        print("count")
+                      
+                        pred_idx_array = np.argmax(pred_prob_array, 1).tolist()
+
+                        if config['ma_flag']:
+                               
+                            pred_prob_list.extend(pred_prob)
+                          
+                            pred_prob_ma_array = np.array(pred_prob_list)
+                            
+                            pred_prob_array_0 = pred_prob_ma_array[:,0]
+                            pred_prob_array_1 = pred_prob_ma_array[:,1]
+                            pred_prob_array_2 = pred_prob_ma_array[:,2]
+                        
+                            pred_prob_ma_array_0 = moving_average(pred_prob_array_0,  config['ma_clip'])
+                            pred_prob_ma_array_1 = moving_average(pred_prob_array_1,  config['ma_clip'])
+                            pred_prob_ma_array_2 = moving_average(pred_prob_array_2,  config['ma_clip'])
+                            
+                             
+                            if len(pred_prob_ma_array_0) == config['clip_num']:
+                                pred_prob_ma = np.zeros((config['clip_num'], 3))
+                               
+                                pred_prob_ma[:,0] =np.array(pred_prob_ma_array_0) 
+                                pred_prob_ma[:,1] =np.array(pred_prob_ma_array_1)
+                                pred_prob_ma[:,2] =np.array(pred_prob_ma_array_2)
+                                
+                                pred_idx_array = np.argmax(pred_prob_ma,1).tolist()
+                        
+                        pred_mean_array = np.mean(pred_prob_array, axis = 0)       
+                                
+                        # print("0 count : ",pred_idx_array.count(0))
+                        # print("1 count : ",pred_idx_array.count(1))
+                        # print("2 count : ",pred_idx_array.count(2))
+
+                        result_list = []
+                        result_list.append(frame_index)
+                        result_list.append(pred_mean_array[0])
+                        result_list.append(pred_mean_array[1])
+                        result_list.append(pred_mean_array[2])
+                        result_list.append(y_label)
+                        wr_n.writerow(result_list)
+                        
+                        print("--------------------------------")
+                       
+                        
+                        pred_prob_list = pred_prob[len(pred_prob)-config['ma_clip']+1:]
+                        pred_prob.clear()
+
+
+                    
+                    #out.write(frame)
+
+                    # ESC를 누르면 종료
+                    key = cv2.waitKey(1) & 0xFF
+                    if (key == 27):
+                        break
+
+                          
+        
+                stomach_flag = False
+                small_bowel_flag = False
+                colon_flag = False
+                cls_display = ""
+
+                cap.release()
+                #out.release()
+                cv2.destroyAllWindows()
+
+                pred_count.clear()
+    print("complete")
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    config = ConfigParser.from_args(args)
+    main(config)
